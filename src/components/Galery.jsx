8d85610ebac9b6e3055fd58cda18ab74435c6ca3
Moneshsoni <sonimonesh115@gmail.com>
2024-01-07 23:50:02 +0530

    Add the Tic-Toc-Toe project

---
+++ src/components/Galery.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {Profile, MyStyle} from './Profile';
+
+const Galery = () => {
+  return (
+    <div>
+    <section>
+      <h1>Amazing scientists</h1>
+      <Profile />
+      <Profile />
+      <Profile />
+      <MyStyle/>
+    </section>
+      
+    </div>
+  )
+}
+
+export default Galery
---
+++ src/components/Profile.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export const Profile = () => {
+  return (
+    <div>
+      <img
+        src="https://i.imgur.com/MK3eW3As.jpg"
+        alt="Katherine Johnson"
+      />
+    </div>
+  )
+}
+
+export const MyStyle=()=>{
+    return(
+        <ul style={{
+            backgroundColor: 'black',
+            color: 'pink'
+          }}>
+            <li>Improve the videophone</li>
+            <li>Prepare aeronautics lectures</li>
+            <li>Work on the alcohol-fuelled engine</li>
+          </ul>
+    )
+}
